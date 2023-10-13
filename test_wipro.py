89d3547da5b39ccc3867b348e52b978f661e8f44
program733 <kapricon733@gmail.com>
2023-10-13 16:58:32 +0530

    wipro

--- test_wipro.py
+++ test_wipro.py
@@ -1,1 +1,33 @@
 print('test_wipro...')
+
+# pushing and pop of element , using class
+
+# class Example:
+#
+#     def __init__(self):
+#         self.tlist = []
+#
+#     def epop(self,x):
+#         self.tlist.remove(x)
+#
+#     def eappend(self,y):
+#         self.tlist.append(y)
+#
+# ex = Example()
+# ex.eappend(1)
+# print(ex.tlist)
+# ex.eappend(2)
+# print(ex.tlist)
+# ex.epop(1)
+# print(ex.tlist)
+
+
+"""
+[1,2,3,4,3,3,5,7]
+identify to output [3]
+output = [1,2,3
+"""
+#
+# tlist = [1,2,3,4,4,3,3,5,7]
+# findodd = lambda x:[i for i in x if (x.count(i))%2!=0]
+# print(findodd(tlist))
